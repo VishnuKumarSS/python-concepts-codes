04aecfb56de2344f8b5551d065c363e83c8a7a13
VishnuKumarSS <vishnukumarss941@gmail.com>
2022-11-19 01:43:39 +0530

    Pushed 10inheritance.py

---
+++ Modules/sample_import.py
@@ -0,0 +1,7 @@
+# importing module, which contains function
+
+import sample # sample is a module name
+a=int(input("Enter a number  "))
+b=int(input("Enter another number  "))
+print(f"Addition: {sample.addi(a,b)}")
+print(f"Subtraction: {sample.subs(a,b)}")
---
+++ OOPS_in_python/10inheritance.py
@@ -0,0 +1,17 @@
+class Person( object ):
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+        print(self.salary)
+        print(self.post)
+class Employee( Person ):
+    def __init__(self, name,idnumber,salary,post):
+        self.salary = salary
+        self.post = post
+        Person.__init__(self, name,idnumber)
+
+obj = Employee('Rahul', 886012, 200000, "Intern")
+obj.display()
